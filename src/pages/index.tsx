df76ddd4ee11c9c865443e86ce5dca088b2e041f
marcioliveira <marcioruelaoliveira@gmail.com>
2021-04-22 10:06:59 -0300

    termino da ultimos episodes na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,6 +44,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   height={192} 
                   src={episode.thumbnail} 
                   alt={episode.title} 
+                  objectFit="cover"
                 />
 
                 <div className={styles.episodeDetails}>
